0ffcfd0d3d6194f7b1dba3d29d817a9fb1b3ac45
AfrozKhan <pathanafrozkhan786@gmail.com>
2025-10-02 16:19:14 +0530

    udation

--- section9/gatewayServer/src/main/java/com/eazybytes/gatewayServer/GatewayServerApplication.java
+++ section9/gatewayServer/src/main/java/com/eazybytes/gatewayServer/GatewayServerApplication.java
@@ -2,6 +2,9 @@ package com.eazybytes.gatewayServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayServerApplication {
@@ -10,4 +13,23 @@ public class GatewayServerApplication {
 		SpringApplication.run(GatewayServerApplication.class, args);
 	}
 
+
+    @Bean
+	public RouteLocator eazyBankRouteConfig(RouteLocatorBuilder routeLocatorBuilder)
+	{
+		return routeLocatorBuilder.routes()
+				.route(p-> p
+						.path("/eazybank/accounts/**")
+						.filters(f->f.rewritePath("/eazybank/accounts/(?<segment>.*)","/${segment}"))
+						.uri("lb://ACCOUNTS"))
+
+				.route(p-> p
+						.path("/eazybank/loans/**")
+						.filters(f->f.rewritePath("/eazybank/accounts/(?<segment>.*)","/${segment}"))
+						.uri("lb://LOANS"))
+				.route(p-> p
+						.path("/eazybank/cards/**")
+						.filters(f->f.rewritePath("/eazybank/accounts/(?<segment>.*)","/${segment}"))
+						.uri("lb://CARDS")).build();
+	}
 }
